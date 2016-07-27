020fd811f422733d4ded4d15cca821afcf319dee
CezaryRynkowski <crynkowski@cludo.pl>
2016-07-27 16:07:54 +0200

    Added method to calling RestAPI and show json result on main page

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.IO;
+using System.Json;
+using System.Net;
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Text.Method;
 
 namespace WarsawTramsOnline
 {
@@ -24,7 +29,42 @@ namespace WarsawTramsOnline
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += async (sender, e) =>
+            {
+                string url = @"https://api.um.warszawa.pl/api/action/wsstore_get/?id=c7238cfe-8b1f-4c38-bb4a-de386db7e776&apikey=f1712a99-0dad-42e1-9f12-c660438bd769";
+                JsonValue json = await Test(url);
+                Display(json);
+            };
+        }
+
+        private async Task<JsonValue> Test(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            // Send the request to the server and wait for the response:
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                // Get a stream representation of the HTTP web response:
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // Use this stream to build a JSON document object:
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+                    // Return the JSON document:
+                    return jsonDoc;
+                }
+            }
+        }
+
+        private void Display(JsonValue json)
+        {
+            TextView result = FindViewById<TextView>(Resource.Id.textView1);
+
+            result.Text = json.ToString();
+            result.MovementMethod = new ScrollingMovementMethod();
         }
     }
 }
